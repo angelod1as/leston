f4baed2a3a5de575e663d2007543c5c3eaf7737d
Angelo Dias <oiangelodias@gmail.com>
2021-08-14 21:52:26 -0300

    Add fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,10 +5,17 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,700;1,700&family=Montserrat:ital,wght@0,300;0,500;0,700;1,300;1,500;1,700&display=swap"
-            rel="stylesheet"
+            rel="preload"
+            href="/fonts/times-italic.otf"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/Union-Regular.ttf"
+            as="font"
+            crossOrigin=""
           />
         </Head>
         <body>
