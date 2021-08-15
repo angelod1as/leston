37502395b183e89bf62c936c85aba3d1c7634b3e
Angelo Dias <oiangelodias@gmail.com>
2021-08-15 14:44:05 -0300

    Add types

--- src/@types/types.ts
+++ src/@types/types.ts
@@ -3,14 +3,22 @@ import { MDXRemoteSerializeResult } from 'next-mdx-remote'
 // API
 export type Credits = {
   name: string
-  role: string
+  roles: string
   gap?: boolean
+}
+
+type Image = {
+  path: string
+  width: number
+  height: number
 }
 
 export type FrontMatter = {
   title: string
   excerpt: string
-  image: string
+  open: boolean
+  extraInfo: string
+  image: Image
   credits: Credits[]
 }
 
