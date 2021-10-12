551e9bd75a165ff3b7c9ccf0e40281d88a2f365a
Angelo Dias <oiangelodias@gmail.com>
2021-10-12 10:56:26 -0300

    Add sidebar spacing

--- src/components/pages/Home/Sidebar.tsx
+++ src/components/pages/Home/Sidebar.tsx
@@ -3,7 +3,7 @@ import LangSwitcher from '@components/LangSwitcher'
 
 export default function Sidebar() {
   return (
-    <div className="fixed z-0 flex -rotate-90 gap-x-8 -right-24 bottom-32">
+    <div className="fixed z-0 flex -rotate-90 gap-x-8 -right-20 bottom-32">
       <ToTop />
       <LangSwitcher />
     </div>
