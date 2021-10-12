82ea6c617662ad4f48649470c585bdafcafa2b0d
Angelo Dias <oiangelodias@gmail.com>
2021-10-12 12:50:02 -0300

    fix about z-index

--- src/components/pages/Home/Footer.tsx
+++ src/components/pages/Home/Footer.tsx
@@ -13,17 +13,17 @@ export default function Footer({
   handleToggleAbout,
 }: Props) {
   return (
-    <div className="fixed bottom-0 left-0 w-full bg-white z-10">
+    <div className="fixed bottom-0 left-0 w-full bg-white z-[100]">
       <button
         onClick={handleToggleAbout}
-        className="w-full h-5 block"
+        className="block w-full h-5"
         style={{
           background:
             'linear-gradient(270deg, #58585B -51.04%, #F0F0F0 -23.96%, #929EA7 63.54%, #0B0C0F 93.75%)',
         }}
       />
       {aboutOpen && (
-        <div className="grid grid-cols-3 my-4 mx-10">
+        <div className="grid grid-cols-3 mx-10 my-4">
           <MDXRemote compiledSource={about.compiledSource} />
           <div className="col-start-3">
             <MDXRemote compiledSource={contact.compiledSource} />
