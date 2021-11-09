e381d0bab9277b1ff6407308c0130ebaff01ab88
Angelo Dias <oiangelodias@gmail.com>
2021-11-09 11:28:01 -0300

    Fix draggable and add easy way to disable

--- src/components/pages/Home/Stone.tsx
+++ src/components/pages/Home/Stone.tsx
@@ -4,19 +4,32 @@ import Draggable from 'react-draggable'
 
 type Props = {
   image: StaticImageData
+  draggable?: boolean
 }
 
-export default function Stone({ image }: Props) {
+export default function Stone({ image, draggable = true }: Props) {
   const ref = useRef(null)
 
   if (image?.src) {
+    if (draggable) {
+      return (
+        <div className="absolute flex items-center justify-center w-full h-screen overflow-hidden">
+          <Draggable nodeRef={ref}>
+            <div className="fixed z-50 cursor-move ">
+              <div className="pointer-events-none">
+                <Image src={image} alt="" className="pointer-events-none" />
+              </div>
+            </div>
+          </Draggable>
+        </div>
+      )
+    }
+
     return (
-      <div className="absolute flex items-center justify-center w-screen h-screen overflow-hidden">
-        <Draggable nodeRef={ref}>
-          <div className="fixed z-50 cursor-move">
-            <Image src={image} alt="" className="pointer-events-none" />
-          </div>
-        </Draggable>
+      <div className="absolute flex items-center justify-center w-full h-screen overflow-hidden">
+        <div className="fixed z-50">
+          <Image src={image} alt="" className="pointer-events-none" />
+        </div>
       </div>
     )
   }
