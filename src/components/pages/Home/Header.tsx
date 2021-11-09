9d64d5bb3624acdcfdc505ea75c7301b8763293a
Angelo Dias <oiangelodias@gmail.com>
2021-11-09 13:14:07 -0300

    Add mobile header

--- src/components/pages/Home/Header.tsx
+++ src/components/pages/Home/Header.tsx
@@ -1,6 +1,7 @@
 import { useLocaleContext } from '@components/LocaleContext/LocaleContext'
 import Image from 'next/image'
 import logo from 'public/images/logo/logo.svg'
+import logoVert from 'public/images/logo/logo-vert.svg'
 
 type Props = {
   handleToggleAbout: () => void
@@ -32,9 +33,9 @@ export default function Header({ handleToggleAbout }: Props) {
   const data = text[locale]
 
   const scroll = (to: string) => {
-    const archive = document.getElementById(to)
-    if (archive) {
-      archive.scrollIntoView({
+    const section = document.getElementById(to)
+    if (section) {
+      section.scrollIntoView({
         block: 'start',
         behavior: 'smooth',
         inline: 'center',
@@ -43,32 +44,73 @@ export default function Header({ handleToggleAbout }: Props) {
   }
 
   return (
-    <div className="relative z-10 flex flex-col items-center justify-between h-screen text-white">
-      <div className="flex items-start justify-between w-full pt-6 px-14">
-        <p>
-          {data.Artist}, {data.Musician},
-          <br />
-          {data['Multimedia Developer']},<br />
-          {data['Creative Technologist']}
-        </p>
-        <button className="times" onClick={() => scroll('archives')}>
-          {data.Archive}
-        </button>
-        <button className="times" onClick={handleToggleAbout}>
-          {data.About}
-        </button>
-        <button className="times" onClick={() => scroll('highlights')}>
-          {data.Highlights}
-        </button>
+    <>
+      {/* Desktop */}
+      <div className="hidden md:block">
+        <div className="relative z-10 flex flex-col items-center justify-between h-screen text-white">
+          <div className="flex items-start justify-between w-full pt-6 px-14">
+            <p>
+              {data.Artist}, {data.Musician},
+              <br />
+              {data['Multimedia Developer']},<br />
+              {data['Creative Technologist']}
+            </p>
+            <button className="times" onClick={() => scroll('archives')}>
+              {data.Archive}
+            </button>
+            <button className="times" onClick={handleToggleAbout}>
+              {data.About}
+            </button>
+            <button className="times" onClick={() => scroll('highlights')}>
+              {data.Highlights}
+            </button>
 
-        <p>
-          hello@matheusleston.com <br />
-          @matheusleston
-        </p>
+            <p>
+              hello@matheusleston.com <br />
+              @matheusleston
+            </p>
+          </div>
+          <div className="mb-3">
+            <Image src={logo} alt="LESTON" />
+          </div>
+        </div>
       </div>
-      <div className="mb-3">
-        <Image src={logo} alt="LESTON" />
+
+      {/* Mobile */}
+      <div className="block md:hidden">
+        <div className="relative z-10 grid h-screen max-h-screen grid-cols-5 gap-4 pt-4 pb-8 text-white">
+          <div className="col-span-2 -mt-4 -ml-1 overflow-hidden">
+            <div className="">
+              <Image src={logoVert} alt="LESTON" />
+            </div>
+          </div>
+          <div className="flex flex-col justify-between">
+            <button className="times" onClick={handleToggleAbout}>
+              {data.About}
+            </button>
+            <div>
+              <button className="times" onClick={() => scroll('archives')}>
+                {data.Archive}
+              </button>
+              <button className="times" onClick={() => scroll('highlights')}>
+                {data.Highlights}
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between col-span-2">
+            <p>
+              {data.Artist}, {data.Musician},
+              <br />
+              {data['Multimedia Developer']},<br />
+              {data['Creative Technologist']}
+            </p>
+            <p className="break-words">
+              hello@matheusleston.com <br />
+              @matheusleston
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
