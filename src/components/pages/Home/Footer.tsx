90049d4842b502dc8b108f7a86e5f830e95c2832
Angelo Dias <oiangelodias@gmail.com>
2021-11-23 08:56:48 -0300

    Add close button

--- src/components/pages/Home/Footer.tsx
+++ src/components/pages/Home/Footer.tsx
@@ -17,7 +17,7 @@ export default function Footer({
 }: Props) {
   return (
     <div
-      className={`fixed bottom-0 left-0 w-full z-[100] max-h-[50%] bg-white overflow-y-scroll `}
+      className={`fixed bottom-0 left-0 w-full z-[100] max-h-[50%] bg-white overflow-y-scroll`}
     >
       <button
         onClick={handleToggleAbout}
@@ -28,7 +28,13 @@ export default function Footer({
         }}
       />
       <Collapse isOpened={aboutOpen}>
-        <div className="mx-10 my-8 md:grid md:grid-cols-4 md:gap-4">
+        <div className="relative mx-10 my-8 md:grid md:grid-cols-4 md:gap-4">
+          <button
+            className="absolute top-0 right-0 flex items-center justify-center w-3 h-3 p-3 text-white transition-opacity bg-black rounded-full hover:opacity-90"
+            onClick={handleToggleAbout}
+          >
+            ×
+          </button>
           <MDXRemote compiledSource={about.compiledSource} />
           <div className="col-start-3 my-8 md:m-0">
             <Image src={leston} alt="" className="z-[-1]" />
@@ -40,30 +46,4 @@ export default function Footer({
       </Collapse>
     </div>
   )
-
-  // return (
-  //   <div
-  //     className={`footer fixed bottom-0 left-0 w-full z-[100] max-h-[50%] bg-white ${
-  //       aboutOpen ? 'h-full overflow-scroll' : 'h-5'
-  //     }`}
-  //   >
-  // <button
-  //   onClick={handleToggleAbout}
-  //   className={`w-full h-5 ${aboutOpen && 'fixed top-1/2 left-0'}`}
-  //   style={{
-  //     background:
-  //       'linear-gradient(270deg, #58585B -51.04%, #F0F0F0 -23.96%, #929EA7 63.54%, #0B0C0F 93.75%)',
-  //   }}
-  // />
-  //   <div className="mx-10 my-8 md:grid md:grid-cols-4 md:gap-4">
-  //     <MDXRemote compiledSource={about.compiledSource} />
-  //     <div className="col-start-3 my-8 md:m-0">
-  //       <Image src={leston} alt="" className="z-[-1]" />
-  //     </div>
-  //     <div className="col-start-4">
-  //       <MDXRemote compiledSource={contact.compiledSource} />
-  //     </div>
-  //   </div>
-  // </div>
-  // )
 }
