7d9e4d670e5dbf7bdcd57f6287717cbe75d96130
Angelo Dias <oiangelodias@gmail.com>
2021-12-15 10:53:15 -0300

    Add ios mobile fix

--- src/components/pages/Home/Header.tsx
+++ src/components/pages/Home/Header.tsx
@@ -27,19 +27,22 @@ const text = {
   },
 }
 
-function isIos() {
-  return (
-    [
-      'iPad Simulator',
-      'iPhone Simulator',
-      'iPod Simulator',
-      'iPad',
-      'iPhone',
-      'iPod',
-    ].includes(navigator.platform) ||
-    // iPad on iOS 13 detection
-    (navigator.userAgent.includes('Mac') && 'ontouchend' in document)
-  )
+const isIos: boolean = () => {
+  if (navigator?.platform) {
+    return (
+      [
+        'iPad Simulator',
+        'iPhone Simulator',
+        'iPod Simulator',
+        'iPad',
+        'iPhone',
+        'iPod',
+      ].includes(navigator.platform) ||
+      // iPad on iOS 13 detection
+      (navigator.userAgent.includes('Mac') && 'ontouchend' in document)
+    )
+  }
+  return false
 }
 
 export default function Header({ handleToggleAbout }: Props) {
