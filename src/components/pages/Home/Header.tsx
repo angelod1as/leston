9b07131e29fffc2f0e4b64f95cde0756467f8f2d
Angelo Dias <oiangelodias@gmail.com>
2022-04-30 15:35:18 +0200

    Change close

--- src/components/pages/Home/Header.tsx
+++ src/components/pages/Home/Header.tsx
@@ -2,10 +2,12 @@ import LangSwitcher from '@components/LangSwitcher'
 import { useLocaleContext } from '@components/LocaleContext/LocaleContext'
 import { MDXRemote } from 'next-mdx-remote'
 import Image from 'next/image'
-import logo from 'public/images/app/logo.svg'
+import logoWhite from 'public/images/app/logo.svg'
+import logoBlack from 'public/images/app/logo-black.svg'
 import { About } from 'src/@types/types'
 import leston from 'public/images/app/leston.png'
 import * as Dialog from '@radix-ui/react-dialog'
+import { FC } from 'react'
 
 type Props = {
   showTitle: boolean
@@ -90,8 +92,8 @@ export default function Header({ showTitle, about }: Props) {
   )
 
   const CloseAbout = () => (
-    <Dialog.Trigger className="absolute grid w-5 text-black bg-white rounded-full top-2 right-2 place-items-center h-5w-5">
-      &#xd7;
+    <Dialog.Trigger className="text-left times hover md:text-center">
+      {data.CloseAbout}
     </Dialog.Trigger>
   )
 
@@ -117,6 +119,24 @@ export default function Header({ showTitle, about }: Props) {
     </div>
   )
 
+  const LestonWrapper: FC = ({ children }) => (
+    <div className="fixed bottom-0 left-0 z-10 px-2 mb-6 md:w-full md:px-10 md:full-img sm:bg-transparent ">
+      {children}
+    </div>
+  )
+
+  const LestonWhite = () => (
+    <LestonWrapper>
+      {showTitle && <Image src={logoWhite} alt="LESTON" className="w-full" />}
+    </LestonWrapper>
+  )
+
+  const LestonBlack = () => (
+    <LestonWrapper>
+      {showTitle && <Image src={logoBlack} alt="LESTON" className="w-full" />}
+    </LestonWrapper>
+  )
+
   return (
     <>
       <div className="relative z-10 flex flex-col items-start justify-between h-screen p-2 overflow-scroll text-white md:items-center">
@@ -129,18 +149,19 @@ export default function Header({ showTitle, about }: Props) {
           <AboutLink />
           <Locale />
         </div>
-        <div className="fixed bottom-0 left-0 z-10 px-2 mb-6 md:w-full md:px-10 md:full-img sm:bg-transparent ">
-          {showTitle && <Image src={logo} alt="LESTON" className="w-full" />}
-        </div>
+        <LestonWhite />
       </div>
 
-      <Dialog.Overlay className="fixed top-0 left-0 right-0 z-50 flex flex-col items-start justify-between h-full p-4 overflow-scroll overflow-y-auto bg-black md:items-center bg-opacity-90">
-        <Dialog.Content className="flex flex-col w-full gap-4 p-2 mb-5 bg-white md:h-full md:grid md:items-start md:grid-cols-4 md:px-10 md:pt-6 ">
-          <CloseAbout />
-          <About1 />
-          <About2 />
-          <Image src={leston} alt="Leston picture" />
-          <Contact />
+      <Dialog.Overlay className="fixed top-0 left-0 right-0 z-50 flex flex-col items-start justify-between h-full overflow-scroll overflow-y-auto bg-white md:items-center">
+        <Dialog.Content className="md:h-full">
+          <div className="flex flex-col w-full gap-4 p-2 mb-5 bg-white md:mb-0 md:h-full md:grid md:items-start md:grid-cols-5 md:px-10 md:pt-8">
+            <About1 />
+            <About2 />
+            <Image src={leston} alt="Leston picture" />
+            <CloseAbout />
+            <Contact />
+          </div>
+          <LestonBlack />
         </Dialog.Content>
       </Dialog.Overlay>
     </>
