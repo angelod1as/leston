f147ac27085c7535149a667ceac6a381e6a8c9e5
Angelo Dias <oiangelodias@gmail.com>
2022-04-30 17:22:48 +0200

    Fix: weird black bg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,7 +40,7 @@ const Index = (props: Props) => {
       <div>
         <div
           style={{
-            backgroundColor: `rgba(240,240,240,${dimension || 0})`,
+            backgroundColor: `rgba(0,0,0,${1 - dimension || 0})`,
           }}
         >
           <Home projects={projects} about={about} showTitle={showTitle} />
