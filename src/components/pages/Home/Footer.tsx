21091a6ceed9374ed63ebb408a6402d26acf0457
Angelo Dias <oiangelodias@gmail.com>
2022-06-20 14:22:33 +0200

    Add styles

--- src/components/pages/Home/Footer.tsx
+++ src/components/pages/Home/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div
-      className={`fixed bottom-0 left-0 w-full z-50 h-5 bg-white overflow-y-scroll`}
+      className={`fixed bottom-0 left-0 w-full z-50 h-5 bg-white`}
       style={{
         background:
           'linear-gradient(270deg, #58585B -51.04%, #F0F0F0 -23.96%, #929EA7 63.54%, #0B0C0F 93.75%)',
